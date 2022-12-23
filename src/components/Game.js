8b70f3a5820dbbfed7b583d5cafa3de7dd22dab4
Augusto Seta <alseta97@gmail.com>
2022-12-23 15:38:10 -0300

    the key attribute was added to the cards.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import data from '../data/data';
 
-const Game = () => {
+const Game = (props) => {
+
+  console.log(props);
 
   return (
     <div className='app-wrapper' >
 
       { data.map( (card, index) => (
-        <div className='game--card' key={ index } >
+        <div className='game--card' key={ index } onClick={ props.handleScore } >
           <img src={ card.img } />
           <h2>{ card.name }</h2>
         </div>
