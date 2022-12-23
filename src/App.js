9f6b430ff0241b560e5f9b429facf7b5e88a2ad0
Augusto Seta <alseta97@gmail.com>
2022-12-23 15:36:53 -0300

    implemented a shuffleData function that reorders the data within the data array and thanks to the useEffect hook, every time the score changes, the cards switch places.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Game from './components/Game';
 import Header from './components/Header';
+import data from './data/data';
 import './styles/style.css'
 
 function App() {
   const [scores, setScore] = useState({ score: 0, bestScore: 0 })
 
+  const incrementScore = () => setScore( { ...scores, score : scores.score + 1 } )
+  const setMaxScore = () => setScore( { ...scores, bestScore : scores.score } )
+
+  const shuffleData = ( data ) => {
+    for ( let i = data.length - 1; i > 0; i -- ) {
+      const j = Math.floor(Math.random() * ( i - 1 ));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+    }
+    return data
+  }
+
+  useEffect(() => {
+    shuffleData(data)
+  }, [scores])
+
   return (
     <div className="container">
       <Header scores={ scores } />
-      <Game />
+      <Game handleScore={ incrementScore } />
     </div>
   );
 }
