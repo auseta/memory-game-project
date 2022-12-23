596d433c19887505de3404d69431f6fe2b08c806
Augusto Seta <alseta97@gmail.com>
2022-12-23 14:23:59 -0300

    A local database was created containing a list containing objects whose keys are the name and images of the characters to be displayed on the game cards.

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+import Solaire from '../images/Solaire.jpg';
+import Gwyn from '../images/Gwyn.jpg';
+import Nito from '../images/Nito.jpg';
+import Patches from '../images/Patches.jpg';
+import Seath from '../images/Seath.jpg';
+import Witch from '../images/Witch.jpg';
+import Oscar from '../images/Oscar.jpg';
+import FourKings from '../images/Four-Kings.jpg';
+import Ornstein from '../images/Ornstein.jpg';
+import Smough from '../images/Smough.jpg';
+
+const data = [
+  {
+    name : 'Solaire of Atora',
+    img : Solaire
+  },
+  {
+    name : 'Gwyn, Lord of Cinder',
+    img : Gwyn
+  },
+  {
+    name : 'Gravelord Nito',
+    img : Nito,
+  },
+  {
+    name : 'Trusty Patches',
+    img : Patches,
+  },
+  {
+    name : 'Seath the Scaleless',
+    img : Seath
+  },
+  {
+    name : 'Witch of Izalith',
+    img : Witch
+  },
+  {
+    name : 'Oscar of Astora',
+    img : Oscar
+  },
+  {
+    name : 'Four Kings',
+    img : FourKings
+  },
+  {
+    name : 'Ornstein',
+    img : Ornstein
+  },
+  {
+    name : 'Smough',
+    img : Smough
+  }
+]
+
+export default data;
