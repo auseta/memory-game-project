c58877aba142d249f23f3c84521b78f4dac311bd
Augusto Seta <alseta97@gmail.com>
2022-12-26 15:22:05 -0300

    minor changes to header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Header = ({ scores }) => {
   return (
     <div className='header'>
-      <h1>Memory Game</h1>
+      <div>
+        <h1>Dark Souls - The Memory Game Edition </h1>
+        <p>The game is quite simple. Select a card and then select another BUT NOT THE SAME! to score points and so on. Enjoy it!</p>
+      </div>
       <div className='header--score'>
-        <h4>Score: { scores.score } </h4>
-        <h4>Best score: { scores.bestScore }</h4>
+        <h3>Score: { scores.score } </h3>
+        <h3>Best score: { scores.bestScore }</h3>
       </div>
     </div>
   )
