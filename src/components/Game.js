f7f5409644f624993dbf5d985ccc3c3e69875253
Augusto Seta <alseta97@gmail.com>
2022-12-26 15:22:48 -0300

    minor changes to the game component.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import data from '../data/data';
 
-const Game = (props) => {
-
-  console.log(props);
+const Game = ({ handleScores }) => {
 
   return (
     <div className='app-wrapper' >
 
       { data.map( (card, index) => (
-        <div className='game--card' key={ index } onClick={ props.handleScore } >
-          <img src={ card.img } />
+        <div className='game--card' key={ index } onClick={ () => handleScores( card.name ) } >
+          <img src={ card.img } alt='card-illustration' />
           <h2>{ card.name }</h2>
         </div>
       ))}
